ce1e3b1bf1d0cfb5e6f88a49cdd02661c933d4aa
tCruz90 <a44340@ualg.pt>
2023-07-03 17:43:24 +0100

    trying to implement zipkin with micrometer. tomorrow will try to implement a new alternative.

--- product/src/main/java/com/tutorial/product/controller/ProductController.java
+++ product/src/main/java/com/tutorial/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.tutorial.product.controller;
 import com.tutorial.product.dto.ProductRequestDTO;
 import com.tutorial.product.dto.ProductResponseDTO;
 import com.tutorial.product.service.ProductService;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
+@Timed
 public class ProductController {
 
     private final ProductService productService;
